2b0a4a8c65664d65ce0c5dbd14be5070f9236c61
dammitBrandon <bailey1.brandon@gmail.com>
2020-01-13 23:55:01 -0800

    able to view a single question set

--- src/components/question-sets/question-set.js
+++ src/components/question-sets/question-set.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {findById} from '../../modules/app/redux/question-sets';
+import { Layout} from "antd";
 
 class QuestionSetPage extends Component {
   
   render() {
+    const {questionSet} = this.props;
+    
     return (
-      <div>
-        QuestionSet Details
-      </div>
-    )
+      <Layout>
+        <h3>{questionSet.title} Details</h3>
+      </Layout>
+    );
   }
 }
-export default QuestionSetPage;
+
+function mapStateToProps(state, ownProps) {
+  let questionSetId = ownProps.match.params.questionSetId;
+  
+  return {
+    questionSet: findById(state.app.questionSets.data, questionSetId)
+  };
+};
+
+export default connect(mapStateToProps, {})(QuestionSetPage);
