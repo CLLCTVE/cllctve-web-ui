d3560d41bdfc34f27c7db733a9698f5f21cbf067
dammitBrandon <bailey1.brandon@gmail.com>
2020-03-21 05:20:11 -0700

    finally able to properly display thrown error message

--- src/modules/auth/sagas.js
+++ src/modules/auth/sagas.js
@@ -1,20 +1,36 @@
-import { all, takeLatest } from 'redux-saga/effects';
-
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 
 import {
   LOGIN_REQUEST,
+  LOGIN_SUCCESS,
 } from './redux';
 
 import { FORM_ERROR } from 'final-form';
+import history from '../../store';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const submit = async values => {
+  console.log('#submit, values: ', values);
+  await sleep(200);
+  if (values.email !== "bailey1.brandon@gmail.com" || values.password !== "abc123") {
+    console.log('email/password is wrong, throwing form error');
+    throw { [FORM_ERROR]: 'Login Failed' }
+  }
+  console.log('#submit, success');
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 function* loginFlow(action) {
   console.log('#loginFlow, action: ', action);
   try {
-    console.error('#loginFlow try block');
+    console.log('#loginFlow try block');
+    const response = yield call(submit, action.payload);
+    console.log('#loginFlow, response: ', response)
     
   } catch(err) {
     console.error('#loginFlow catch block, err: ', err);
-    
+    yield put({type: LOGIN_SUCCESS, payload: err})
   }
 }
 
