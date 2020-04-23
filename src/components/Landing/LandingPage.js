69711eb0a49fae40c50b33876f9432c113ff9fff
MLHLocalhost <localhost@mlh.io>
2020-04-23 12:19:47 -0400

    update

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -133,21 +133,47 @@ render() {
   
 
   <body>
+
+    
     <article>
       <div id="mainimg"></div>
+      <div class="enterbar">
       <form id="mainemail"class="container"ng-submit="home.submitEmail(home.app.email)">
         <input type="text" name="email" placeholder="Email"ng-model="home.app.email"ng-required="true" />
         <button type="submit">Enter</button>
       </form>
+      </div>
     </article>
 
     <article id="news">
       <div class="container">
         <h1 ui-sref="news">Latest News Stories</h1>
-        <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
-          <h3 ng-bind-html="x.title" class="ng-binding">The British Invasion: The Growing Crossover Between Content in the US &amp; UK</h3>
+        <div>
+          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+            <h3>MAKE YOUR SPRING BREAK WORTHWHILE</h3>
+            <div id="mainimg"></div>
+          </div>
+          
+
+          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+            <h3>The British Invasion: The Growing Crossover Between Content in the US &amp; UK</h3>
+            <div id="mainimg"></div>
+          </div>
+          
+
+          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+            <h3>UNINSPIRED? WE’VE GOT THE FIX!</h3>
+            <div id="mainimg"></div>
+          </div>
+          
+
+          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+            <h3>20 BLACK CREATIVES TO WATCH IN NEW YORK</h3>
+            <div id="mainimg"></div>
+          </div>
+          
+
         </div>
-        <div id="mainimg"></div>
         </div>
     </article>
 
@@ -160,7 +186,8 @@ render() {
                 {/* according to app.js in landing page we want the zero cell of this json list*/}
                 {index==0 &&
                  <h1>
-                    {item.content.rendered} 
+                    {/*{item.content.rendered} */}
+                    A PLATFORM FOR COLLEGE CREATIVES TO CONNECT
                  </h1>
                 }
               </div>
@@ -170,7 +197,9 @@ render() {
         </section>
         <div class="desktopflex">
           <section>
-            <iframe src="https://www.youtube.com/embed/KNU8vKGu_Kg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="video">
+              <iframe src="https://www.youtube.com/embed/KNU8vKGu_Kg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
           </section>
           <section>
           {this.state.creatives.map(function(item,index){
@@ -180,7 +209,8 @@ render() {
                 {index==1 &&
                  <p>
                     {/* home.aboutp */}
-                    {item.content.rendered} 
+                  {/*{item.content.rendered}*/} 
+                  Our vision is to empower the next generation of creatives. We bridge the gap between creatives and brands by providing a platform for college students to develop their portfolio, showcase their skills, and increase their visibility, while providing brands exclusive access to local Gen-Z consumers and Gen-Z content creators.
                  </p>
                 }
               </div>
@@ -211,7 +241,10 @@ render() {
                     
                     <section id="homeProfileImg">
                       {/* home.profileIMg */}
-                      <img src={item.acf.image}/>
+                      
+                  
+                        <img src={item.acf.image}/>
+                  
                     </section>
                   </>
                   }
@@ -229,33 +262,172 @@ render() {
           <div>
             <h1
               ng-bind-html="home.connect">
+                CONNECT WITH OTHER CREATIVES
             </h1>
             <section>
               <div
                 class="othercreative"
                 ng-repeat="x in home.otherCreatives">
                 {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
+                <div
+                  class="creativeimage">
+                  {/*style="background-image: url(https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/brandon-williams.jpg)">*/}
+                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/brandon-williams.jpg"/>
+                </div>
                 <div>
-                  class="creativeimage"
+                  {/* from getothercreatives => <h3>{{x.name}}</h3>*/}
+                  <h3>Brandon Willaims</h3>
+                  {/*<p>{{x.school}}</p>*/}
+                  <p>University of Buffalo '20</p>
+                </div>
+              </div>	
+            </section>
+
+            <section>
+              <div
+                class="othercreative"
+                ng-repeat="x in home.otherCreatives">
+                {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
+                <div
+                  class="creativeimage">
                   {/* fix style="background-image: url({{x.image}})"> */}
+                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/IMG_0086.jpg"/>
                 </div>
                 <div>
                   
                   {/* from getothercreatives => <h3>{{x.name}}</h3>*/}
+                  <h3>AMY NAKAMURA</h3>
                   {/*<p>{{x.school}}</p>*/}
+                  <p>Syracuse University '21</p>
+                </div>
+              </div>	
+            </section>
+
+
+            <section>
+              <div
+                class="othercreative"
+                ng-repeat="x in home.otherCreatives">
+                {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
+                <div
+                  class="creativeimage">
+                  {/* fix style="background-image: url({{x.image}})"> */}
+                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/miles.jpg"/>
+                </div>
+                <div>
+                  
+                  {/* from getothercreatives => <h3>{{x.name}}</h3>*/}
+                  <h3>MILES FRANKLYN</h3>
+                  {/*<p>{{x.school}}</p>*/}
+                  <p>Rutgers University '21</p>
+                </div>
+              </div>	
+            </section>
+           
+            <section>
+              <div
+                class="othercreative"
+                ng-repeat="x in home.otherCreatives">
+                {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
+                <div
+                  class="creativeimage">
+                  {/* fix style="background-image: url({{x.image}})"> */}
+                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/siyaka.jpg"/>
+                </div>
+                <div>
+                  
+                  {/* from getothercreatives => <h3>{{x.name}}</h3>*/}
+                  <h3>SIYAKA TAYLOR-LEWIS</h3>
+                  {/*<p>{{x.school}}</p>*/}
+                  <p>Syracuse University '20</p>
                 </div>
               </div>	
             </section>
           </div>
 
-          <div>
-            <img src="{{home.connectImage}}" alt=""/>
+          <div class ="test">
+            <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/laptop_mockup.png"/>
           </div>
         </>
+      </div>
+    </article>
+
+    <article id="skills">
+      <section>
+        <div class="container">
+          <div>TRACK YOUR SKILLS AND ENGAGE WITH BRANDS
+            <p>As creatives post new content, update their skills, and engage with the platform, they will increase their visibility to be seen by our brand partners looking for fresh young talent to hire for summer internships andlocal campaigns.</p>
+          </div>
+          <div class="test">
+            <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/Copy-of-IMG_9864.jpeg" alt=""/>
+          </div>        
+        </div>
+      </section>
+    </article>
+
+    <article>
+      <div class="container desktopflex">
+        <div class="formtext">
+          <div 
+            class="checkmark"
+            ng-hide="brands.app.viewtype == 'xs' || brands.app.viewtype == 'sm'">
+          </div>
+          <h1>Stay Updated</h1>
+        </div>
+
+        <form
+          id="mainform"
+          name="home.myform" 
+          ng-submit="home.register(creatives.form)" 
+          novalidate>
+          <fieldset>
+           
+            <input type="text" 
+              name="firstname" 
+              placeholder="First Name"
+              ng-model="home.form.firstname"
+              ng-required="true"/>
+            <input type="text" 
+              name="lastname" 
+              placeholder="Last Name"
+              ng-model="home.form.lastname"
+              ng-required="true"/>
+            <input type="text" 
+              name="email" 
+              placeholder="Email"
+              ng-model="home.form.email"
+              ng-required="true"/>
+
+            <input type="text" 
+              name="major" 
+              placeholder="Major"
+              ng-model="home.form.major"
+              ng-required="true"/>
+
+         
+            <input type="text" 
+              name="skills" 
+              placeholder="List your skills (separated by a comma)"
+              ng-model="home.form.skills"
+              ng-required="true"/>
+          </fieldset>
+
+          <button type="submit"
+            ng-click="home.submit()">
+            Join the Waitlist
+          </button>
+        </form>
 
       </div>
-      
     </article>
+
+    <footer class="ng-isolate-scope"><h3>
+
+      2019 CLLCTVE, LLC
+    </h3>
+
+    </footer>
+    
 
   </body>
     
