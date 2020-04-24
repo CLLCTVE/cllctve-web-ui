edb2b5fe85ba969f458ee2e63ef5b3f86d035c08
MLHLocalhost <localhost@mlh.io>
2020-04-23 22:14:39 -0400

    final

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -1,23 +1,6 @@
 import React, {Component} from 'react';
+
 //import './style/landing.css';
-
-import '../style/components/button.scss'
-import '../style/components/footer.scss'
-import '../style/components/form.scss'
-import '../style/components/form.scss'
-import '../style/components/nav.scss'
-
-import '../style/pages/brands.scss'
-import '../style/pages/creatives.scss'
-import '../style/pages/home.scss'
-import '../style/pages/news.scss'
-import '../style/pages/newsstory.scss'
-
-import '../style/styles/breakpoints.scss'
-import '../style/styles/normalize.scss'
-
-import '../style/visuals/colors.scss'
-import '../style/visuals/typography.scss'
 
 
 
@@ -91,7 +74,7 @@ class LandingPage extends Component{
     }
   }
   */
-  //from wpdata.js from landing-page
+  //from wpdata.js from landing-page--api call
   getCreatives(){
     
     //using constructor
@@ -102,7 +85,7 @@ class LandingPage extends Component{
     .then(results => this.setState({'creatives': results}))
   }
 
-  //from wpdata.js from landing-page
+  //from wpdata.js from landing-page --api call
   getPost(){
     
     //using constructor
@@ -114,7 +97,7 @@ class LandingPage extends Component{
     
   }
 
-  //from wpdata.js from landing-page
+  //from wpdata.js from landing-page--api call
   getotherCreatives(){
 
     //using constructor
@@ -135,12 +118,13 @@ render() {
   <body>
 
     
+
     <article>
       <div id="mainimg"></div>
       <div class="enterbar">
-      <form id="mainemail"class="container"ng-submit="home.submitEmail(home.app.email)">
-        <input type="text" name="email" placeholder="Email"ng-model="home.app.email"ng-required="true" />
-        <button type="submit">Enter</button>
+      <form id="mainemail"class="container">
+        <input type="text" name="email" placeholder="Email"/>
+        <button>Enter</button>
       </form>
       </div>
     </article>
@@ -149,27 +133,35 @@ render() {
       <div class="container">
         <h1 ui-sref="news">Latest News Stories</h1>
         <div>
-          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+          <div class="newsinstance">
             <h3>MAKE YOUR SPRING BREAK WORTHWHILE</h3>
-            <div id="mainimg"></div>
+            <div>
+              <img style={{height: '100%', width: '100%'}} src="https://cllctvewp.cecildunston.com/wp-content/uploads/2020/03/vicko-mozara-m82uh_vamhg-unsplash.jpg"/>
+            </div>
           </div>
           
 
-          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+          <div class="newsinstance">
             <h3>The British Invasion: The Growing Crossover Between Content in the US &amp; UK</h3>
-            <div id="mainimg"></div>
+            <div>
+            <img style={{height: '100%', width: '100%'}} src="https://cllctvewp.cecildunston.com/wp-content/uploads/2020/03/union-jack.png"/>
+            </div>
           </div>
           
 
-          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+          <div class="newsinstance">
             <h3>UNINSPIRED? WE’VE GOT THE FIX!</h3>
-            <div id="mainimg"></div>
+            <div>
+            <img style={{height: '100%', width: '100%'}} src="https://cllctvewp.cecildunston.com/wp-content/uploads/2020/03/negative-space-woman-writing-notepad.jpg"/>
+            </div>
           </div>
           
 
-          <div class="newsinstance"  ui-sref="newsstory({story: x.slug})">
+          <div class="newsinstance">
             <h3>20 BLACK CREATIVES TO WATCH IN NEW YORK</h3>
-            <div id="mainimg"></div>
+            <div>
+            <img style={{height: '100%', width: '100%'}} src="https://cllctvewp.cecildunston.com/wp-content/uploads/2020/02/NY-BHM-Creatives-2020.jpg)"/>
+            </div>
           </div>
           
 
@@ -180,44 +172,19 @@ render() {
     <article id="about">
       <div class="container">
         <section>
-          {this.state.creatives.map(function(item,index){
-            return(
-              <div key={index}>
-                {/* according to app.js in landing page we want the zero cell of this json list*/}
-                {index==0 &&
-                 <h1>
-                    {/*{item.content.rendered} */}
-                    A PLATFORM FOR COLLEGE CREATIVES TO CONNECT
-                 </h1>
-                }
-              </div>
-            )
-            }
-          )}
+           <h1>
+              {/*{item.content.rendered} */}
+              A PLATFORM FOR COLLEGE CREATIVES TO CONNECT
+           </h1> 
         </section>
         <div class="desktopflex">
           <section>
             <div class="video">
-              <iframe src="https://www.youtube.com/embed/KNU8vKGu_Kg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe src="https://www.youtube.com/embed/KNU8vKGu_Kg"></iframe>
             </div>
           </section>
-          <section>
-          {this.state.creatives.map(function(item,index){
-            return(
-              <div key={index}>
-                {/* we want cell 1 of the creatives call */}
-                {index==1 &&
-                 <p>
-                    {/* home.aboutp */}
-                  {/*{item.content.rendered}*/} 
-                  Our vision is to empower the next generation of creatives. We bridge the gap between creatives and brands by providing a platform for college students to develop their portfolio, showcase their skills, and increase their visibility, while providing brands exclusive access to local Gen-Z consumers and Gen-Z content creators.
-                 </p>
-                }
-              </div>
-            )
-            }
-          )}
-          </section>
+            <p>Our vision is to empower the next generation of creatives. We bridge the gap between creatives and brands by providing a platform for college students to develop their portfolio, showcase their skills, and increase their visibility, while providing brands exclusive access to local Gen-Z consumers and Gen-Z content creators.
+            </p>
         </div>
       </div>
     </article>
@@ -225,33 +192,18 @@ render() {
     <article id="profile">
       <div class="container">
         <div class="desktopflex">
-          
-          {this.state.creatives.map(function(item,index){
-            return(
-              <div key={index}>
-                  {index==2 &&
-                  <>
-                    <section class="desktopflexbigger">
-                      <h1 ng-bind-html="profile" class="ng-binding">
-                        <h1>Create a profile, and share your interactive portfolio</h1>
-                      </h1>
-                      <p ng-bind-html="profileText" class="ng-binding">During the Spring 2020 semester, CLLCTVE is launching a private beta for college creatives where students will be able to register for an account, create an interactive portfolio, publish new content and job experiences, as well as connect with other creatives directly on the platform.
-                      </p>
-                    </section>
+        
+          <section class="desktopflexbigger">
+            <h1>Create a profile, and share your interactive portfolio</h1>
+              <p>During the Spring 2020 semester, CLLCTVE is launching a private beta for college creatives where students will be able to register for an account, create an interactive portfolio, publish new content and job experiences, as well as connect with other creatives directly on the platform.
+              </p>
+          </section>
                     
-                    <section id="homeProfileImg">
-                      {/* home.profileIMg */}
-                      
-                  
-                        <img src={item.acf.image}/>
-                  
-                    </section>
-                  </>
-                  }
-              </div>
-            )
-            }
-          )}
+          <section id="homeProfileImg">
+            {/* home.profileIMg */}
+            <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/profile_mockup-1.png"/>
+          </section>
+        
         </div>
       </div>
     </article>
@@ -260,19 +212,16 @@ render() {
       <div class="container desktopflex">
         <>
           <div>
-            <h1
-              ng-bind-html="home.connect">
+            <h1>
                 CONNECT WITH OTHER CREATIVES
             </h1>
             <section>
               <div
-                class="othercreative"
-                ng-repeat="x in home.otherCreatives">
+                class="othercreative">
                 {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
-                <div
-                  class="creativeimage">
+                <div class="creativeimage">
                   {/*style="background-image: url(https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/brandon-williams.jpg)">*/}
-                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/brandon-williams.jpg"/>
+                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/brandon-williams.jpg" />
                 </div>
                 <div>
                   {/* from getothercreatives => <h3>{{x.name}}</h3>*/}
@@ -282,11 +231,10 @@ render() {
                 </div>
               </div>	
             </section>
-
+           
             <section>
               <div
-                class="othercreative"
-                ng-repeat="x in home.otherCreatives">
+                class="othercreative">
                 {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
                 <div
                   class="creativeimage">
@@ -306,13 +254,12 @@ render() {
 
             <section>
               <div
-                class="othercreative"
-                ng-repeat="x in home.otherCreatives">
+                class="othercreative">
                 {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
                 <div
                   class="creativeimage">
                   {/* fix style="background-image: url({{x.image}})"> */}
-                  <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/miles.jpg"/>
+                  <img src='https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/miles.jpg'/>
                 </div>
                 <div>
                   
@@ -325,12 +272,9 @@ render() {
             </section>
            
             <section>
-              <div
-                class="othercreative"
-                ng-repeat="x in home.otherCreatives">
+              <div class="othercreative">
                 {/*<!-- <img src="{{x.image}}" alt=""> -->*/}
-                <div
-                  class="creativeimage">
+                <div class="creativeimage">
                   {/* fix style="background-image: url({{x.image}})"> */}
                   <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/siyaka.jpg"/>
                 </div>
@@ -345,8 +289,8 @@ render() {
             </section>
           </div>
 
-          <div class ="test">
-            <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/laptop_mockup.png"/>
+          <div>
+            <img src="https://cllctvewp.cecildunston.com/wp-content/uploads/2019/09/laptop_mockup.png" alt=""/>
           </div>
         </>
       </div>
@@ -369,8 +313,7 @@ render() {
       <div class="container desktopflex">
         <div class="formtext">
           <div 
-            class="checkmark"
-            ng-hide="brands.app.viewtype == 'xs' || brands.app.viewtype == 'sm'">
+            class="checkmark">
           </div>
           <h1>Stay Updated</h1>
         </div>
@@ -378,42 +321,36 @@ render() {
         <form
           id="mainform"
           name="home.myform" 
-          ng-submit="home.register(creatives.form)" 
           novalidate>
+
           <fieldset>
            
             <input type="text" 
               name="firstname" 
               placeholder="First Name"
-              ng-model="home.form.firstname"
               ng-required="true"/>
+
             <input type="text" 
               name="lastname" 
               placeholder="Last Name"
-              ng-model="home.form.lastname"
               ng-required="true"/>
             <input type="text" 
               name="email" 
               placeholder="Email"
-              ng-model="home.form.email"
               ng-required="true"/>
 
             <input type="text" 
               name="major" 
               placeholder="Major"
-              ng-model="home.form.major"
               ng-required="true"/>
-
          
             <input type="text" 
               name="skills" 
               placeholder="List your skills (separated by a comma)"
-              ng-model="home.form.skills"
               ng-required="true"/>
           </fieldset>
 
-          <button type="submit"
-            ng-click="home.submit()">
+          <button type="submit">
             Join the Waitlist
           </button>
         </form>
@@ -421,8 +358,8 @@ render() {
       </div>
     </article>
 
-    <footer class="ng-isolate-scope"><h3>
-
+    <footer>
+    <h3>
       2019 CLLCTVE, LLC
     </h3>
 
