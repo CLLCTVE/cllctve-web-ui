338c90b4d146e9bedaf1285ae3a0dd8e38b80c9c
dammitBrandon <bailey1.brandon@gmail.com>
2020-05-08 14:13:51 -0700

    idk

--- src/components/onboarding-flow/index.js
+++ src/components/onboarding-flow/index.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import MakeAsyncFunction from 'react-redux-promise-listener';
-import {promiseListener} from '../../store';
 import Wizard from './onboarding-flow-wizard';
 import {EducationForm} from './EducationForm';
 import {ExperienceForm} from './ExperienceForm';
@@ -9,24 +7,24 @@ import {SkillsForm} from './SkillsForm';
 import {HonorsAwardsForm} from './HonorsAwardsForm';
 import {LicensesCertsForm} from './LicensesCertsForm';
 import {CenteredContainer as Container} from '../fields/renderFields';
-import { ONBOARDING_FAILURE, ONBOARDING_REQUEST, ONBOARDING_SUCCESS } from '../../modules/signup/redux';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const onSubmit = async values => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 class OnBoardingFlowPage extends Component {
  
   render() {
-    const {match} = this.props;
+    const {isLoading} = this.props;
     
     return (
       <Container>
         <div style={{padding: 24, textAlign: 'center', width: '60vw'}}>
-          <MakeAsyncFunction
-            listener={promiseListener}
-            start={ONBOARDING_REQUEST}
-            resolve={ONBOARDING_SUCCESS}
-            reject={ONBOARDING_FAILURE}
-          >
-            {onSubmit => (
               <Wizard
+                isLoading={isLoading}
                 initialValues={{
                   education: {
                     isEnrolled: false
@@ -38,7 +36,6 @@ class OnBoardingFlowPage extends Component {
                     canExpire: false
                   }
                 }}
-                onSubmit={onSubmit}
               >
                 <Wizard.Page>
                   <EducationForm />
@@ -57,7 +54,6 @@ class OnBoardingFlowPage extends Component {
                 </Wizard.Page>
               </Wizard>
             )}
-          </MakeAsyncFunction>
         </div>
       </Container>
     )
